cfef6f37437e462e3531320bacb3826e1dc4491d
Jon <71294685+october464@users.noreply.github.com>
2020-10-01 17:45:50 -0400

    it

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -55,25 +55,3 @@
     $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
 
-
-//Email.js form
-//const btn = document.getElementById('button');
-
-//document.getElementById('form')
-//    .addEventListener('submit', function (event) {
-//        event.preventDefault();
-
-//        btn.value = 'Sending...';
-
-//        const serviceID = 'default_service';
-//        const templateID = 'template_hked09j';
-
-//        emailjs.sendForm(serviceID, templateID, this)
-//            .then(() => {
-//                btn.value = 'Send Email';
-//                alert('Sent!');
-//            }, (err) => {
-//                btn.value = 'Send Email';
-//                alert(JSON.stringify(err));
-//            });
-//    });
